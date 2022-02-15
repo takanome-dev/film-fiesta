53f53959d2bf30ecdd95781118a1a22cd838e1f3
takanome_dev <takanomedev221@gmail.com>
2022-02-15 13:43:12 +0000

    [fix]: add position fixed to the overlay

--- src/components/styles/Sidebar.styled.ts
+++ src/components/styles/Sidebar.styled.ts
@@ -3,12 +3,11 @@ import styled from "styled-components";
 const Navigation = styled.nav`
 	.overlay {
 		display: none;
-		position: absolute;
+		position: fixed;
 		z-index: 2;
 		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
+		width: 100%;
+		height: 100%;
 		background-color: rgba(0, 0, 0, 0.5);
 	}
 
