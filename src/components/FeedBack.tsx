31cb3cd64ecf4878bd133219a80fc2b76d21743a
takanome_dev <takanomedev221@gmail.com>
2022-02-18 13:09:45 +0000

    [feat]: handle feedback page

--- src/components/FeedBack.tsx
+++ src/components/FeedBack.tsx
@@ -1,8 +1,19 @@
+import Container from "./styles/Feedback.styled";
+
 const FeedBack = () => {
 	return (
-		<div>
-			<h1>FeedBack</h1>
-		</div>
+		<Container className="form">
+			<h1>Give Us Feedback</h1>
+			<fieldset>
+				<label htmlFor="subject">Subject</label>
+				<input className="input" id="subject" type="text" required />
+				<label htmlFor="textarea">Comments</label>
+				<textarea id="textarea" className="input" required />
+			</fieldset>
+			<button className="btn" type="submit">
+				Send my feedback
+			</button>
+		</Container>
 	);
 };
 
