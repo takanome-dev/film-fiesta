814eb771bc35e9d91697c2f41ba511b2218c82d0
takanome_dev <takanomedev221@gmail.com>
2022-03-06 13:42:41 +0000

    [style]: display error message in a tooltip

--- src/components/styles/Input.styled.ts
+++ src/components/styles/Input.styled.ts
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import { appear } from "./Global.styled";
 
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	margin-bottom: 1.2rem;
 	width: 100%;
+	position: relative;
 
 	.label {
 		font-size: 1.1rem;
@@ -25,13 +27,34 @@ const Container = styled.div`
 	}
 
 	.error {
-		margin-top: 1rem;
+		/* margin-top: 1rem; */
 		font-size: 1rem;
 		color: var(--color-primary);
+		position: absolute;
+		background-color: #fff;
+		box-shadow: 0 0.1rem 2rem rgba(0, 0, 0, 0.1);
+		padding: 0.3rem 0.8rem;
+		border-radius: 0.5rem;
+		bottom: -65%;
+		z-index: 2;
+		animation: ${appear} 700ms;
+
 		&::before {
 			content: "⚠️";
 			color: var(--color-primary);
 			margin-right: 0.2rem;
+			font-size: 1.5rem;
+		}
+
+		&::after {
+			content: " ";
+			position: absolute;
+			bottom: 100%;
+			left: 50%;
+			border-width: 0.5rem;
+			margin-left: -0.5rem;
+			border-style: solid;
+			border-color: transparent transparent #fff transparent;
 		}
 	}
 `;
