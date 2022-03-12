fac1c410303ff0311b26eb933f190d11fa5ed998
takanome_dev <takanomedev221@gmail.com>
2022-03-12 10:00:45 +0000

    [refactor]: update feedback service

--- src/services/feedback.ts
+++ src/services/feedback.ts
@@ -1,7 +1,7 @@
 import { FeedbackType } from "../components/types";
 import { http } from "./http";
 
-const endpoint = `${process.env.REACT_APP_API_URL}/feedback`;
+const endpoint = `${process.env.REACT_APP_API_URL}/feedbacks`;
 
 export const sendFeedback = async (feedback: FeedbackType) => {
 	const { data } = await http.post(endpoint, feedback);
