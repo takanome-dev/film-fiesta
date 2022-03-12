771edcba9e7a4530508b0a59aee06247f096d109
takanome_dev <takanomedev221@gmail.com>
2022-03-12 14:40:59 +0000

    [refactor]: update profile service

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,6 +1,6 @@
 import { http } from "./http";
 
-const endpoint = `${process.env.REACT_APP_API_URL}/image`;
+const endpoint = `${process.env.REACT_APP_API_URL}/profile`;
 
 export const updateProfile = async (file: FormData) => {
 	const { data } = await http.post(endpoint, file);
