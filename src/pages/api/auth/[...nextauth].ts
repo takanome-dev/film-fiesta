280b44ec51f664360cf39b7082cba96bac46cedd
takanome_dev <takanomedev221@gmail.com>
2023-03-01 19:40:32 +0000

    it's working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,8 +9,6 @@ import GoogleProvider from 'next-auth/providers/google';
 import { env as clientEnv } from '@/env/client.mjs';
 import { env as serverEnv } from '@/env/server.mjs';
 
-// import type { LoginSchema } from '@/schemas/user';
-
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   debug: serverEnv.NODE_ENV === 'development',
@@ -77,26 +75,6 @@ export const authOptions: NextAuthOptions = {
       },
       from: serverEnv.EMAIL_FROM,
     }),
-    // CredentialsProvider({
-    //   // The name to display on the sign in form (e.g. "Sign in with...")
-    //   name: 'Credentials',
-    //   // `credentials` is used to generate a form on the sign in page.
-    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   // You can pass any HTML attribute to the <input> tag through the object.
-    //   credentials: {},
-    //   async authorize(credentials: LoginSchema, req) {
-    //     // Add logic here to look up the user from the credentials supplied
-    //     const { email, password } = credentials;
-    //     if (email !== 'test@test.com' || password !== 'password') {
-    //       // Any object returned will be saved in `user` property of the JWT
-    //       throw new Error('Invalid email or password');
-    //     }
-    //     return null;
-
-    //     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-    //   },
-    // }),
     /**
      * ...add more providers here
      *
